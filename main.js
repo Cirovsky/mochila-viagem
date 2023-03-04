04c85e8966c3376f54d5a90118ceb659641594cc
Cirovsky <cirovisqui@gmail.com>
2023-03-04 15:32:03 -0300

    lógica de adicionar itens no html (createElement)

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
 /* classes */
 
 class item {
-    constructor(nome, qtd){
+    constructor(nome, qtd) {
         this.nome = nome;
         this.qtd = qtd;
     }
 }
 /* variáveis globais */
-const itens = [];
-const camisa = new item("camisa", 8)
-itens.push(camisa);
+const mochila = [];
 
 const form = document.querySelector("#novoItem");
 
+const lista = document.querySelector(".lista")
+console.log(lista)
+
 /* funções */
-const addItem = () =>{
+const addItem = () => {
     const nomeItem = document.querySelector("#nome").value;
     const qtdItem = parseInt(document.querySelector("#quantidade").value);
-    const novoItem = new item(nomeItem, qtdItem);
-    console.log(novoItem)
-    itens.push(novoItem);
+
+    if(nomeItem == '' || qtdItem == ''){
+        console.log("nenhum item adicionado à mochila");
+    }else{
+        const novoItem = new item(nomeItem, qtdItem);
+        mochila.push(novoItem);
+        addLista(novoItem);
+    }
 }
 
-form.addEventListener("submit", (e) =>{
+const addLista = (item) =>{
+    const novoListItem = document.createElement("li");
+    const novoStrongNumber = document.createElement("strong");
+    novoStrongNumber.textContent = `${item.qtd}`;
+    novoListItem.classList.add("item");
+    novoListItem.append(novoStrongNumber);
+    novoListItem.insertAdjacentText("beforeend", item.nome);
+    lista.append(novoListItem);
+}
+
+const clearForm = () => {
+    document.querySelector("#nome").value = '';
+    document.querySelector("#quantidade").value = '';
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     addItem();
-    console.log("funfou");
-}
-);
-
-
-/* testes */
+    clearForm();
+});
